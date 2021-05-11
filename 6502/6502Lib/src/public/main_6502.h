ebf878193a6d113d5293da2bd1433b326dcc1f32
tiansongyu <478880214@qq.com>
2021-05-11 23:09:59 +0800

    clean code

--- 6502/6502Lib/src/public/main_6502.h
+++ 6502/6502Lib/src/public/main_6502.h
@@ -3,11 +3,17 @@
 #include <stdlib.h>
 
 // http://www.obelisk.me.uk/6502/
-using Byte = unsigned char;
-using Word = unsigned short;
-using u32 = unsigned int;
-using s32 = signed int;
-struct Mem
+namespace m6502
+{
+	using Byte = unsigned char;
+	using Word = unsigned short;
+	using u32 = unsigned int;
+	using s32 = signed int;
+
+	struct Mem;
+	struct CPU;
+}
+struct m6502::Mem
 {
 	static constexpr u32 MAX_MEM = 1024 * 64;
 	Byte Data[MAX_MEM];
@@ -31,7 +37,7 @@ struct Mem
 		Cycles -= 2;
 	}
 };
-struct CPU
+struct m6502::CPU
 {
 	Word PC;
 	Word SP;
@@ -103,7 +109,8 @@ struct CPU
 		return Data;
 	}
 
-	static constexpr Byte   INS_LDA_IM = 0xA9,
+	static constexpr Byte   //LDA
+							INS_LDA_IM = 0xA9,
 							INS_LDA_ZP = 0xA5,
 							INS_LDA_ZPX = 0xB5,
 							INS_LDA_ABS = 0xAD,
@@ -111,131 +118,45 @@ struct CPU
 							INS_LDA_ABS_Y = 0xB9,
 							INS_LDA_IND_X = 0x61,
 							INS_LDA_IND_Y = 0xB1,
+							//LDX
+							INS_LDX_IM = 0xA2,
+							INS_LDX_ZP = 0xA6,
+							INS_LDX_ZPY = 0xB6,
+							INS_LDX_ABS = 0xAE,
+							INS_LDX_ABS_Y = 0xBE,
+							// LDY
+							INS_LDY_IM = 0xA0,
+							INS_LDY_ZP = 0xA4,
+							INS_LDY_ZPX = 0xB4,
+							INS_LDY_ABS = 0xAC,
+							INS_LDY_ABS_X = 0xBC,
+							
 						    INS_JSR = 0x20;
 
-	void LDASetStatus()
+	void SetStatus(const m6502::Byte _register)
 	{
-		Z = (A == 0);
-		N = (A & 0b10000000) > 0;
+		Z = (_register == 0);
+		N = (_register & 0b10000000) > 0;
 	}
 
-	s32 Execute(s32 Cycles, Mem& memory)
-	{
-		s32 CyclesRequests = Cycles;
-		while (Cycles > 0)
-		{
-			Byte Ins = FetchByte(Cycles, memory);
-			switch (Ins)
-			{
-				case INS_LDA_IM: //立即寻址 Immediate
-				{
-					Byte Value = FetchByte(Cycles, memory);
-					A = Value;
-					LDASetStatus();
-				}break;
-				case INS_LDA_ZP: //零页寻址 ..zero page
-				{
-					Byte ZeroPageAddress = FetchByte(Cycles, memory);
-					A = ReadByte(ZeroPageAddress, Cycles, memory);
-					LDASetStatus();
-				}break;
-				case INS_LDA_ZPX: //零页寻址 ..zero page x
-				{
-					Byte ZeroPageAddress = FetchByte(Cycles, memory);
-					ZeroPageAddress += X;
-					Cycles--;
-					A = ReadByte(
-						ZeroPageAddress,
-						Cycles,
-						memory);
-					LDASetStatus();
-				}break;
-				case INS_LDA_ABS:
-				{
-					Word AbsAddress = FetchWord(Cycles, memory);
-					A = ReadByte(
-						AbsAddress,
-						Cycles,
-						memory);
-					LDASetStatus();
-				}break;
-				case INS_LDA_ABS_X:
-				{
-					Word AbsAddress = FetchWord(Cycles, memory);
-					auto Copy_AbsAddress = AbsAddress % 256;
-					// 下面有更好的写法，这里暂时这样写 https://github.com/davepoo/6502Emulator/blob/bb278fbf80d6e4f0f3bb60826afc2551977d9ead/6502/6502Lib/src/private/m6502.cpp#L1068 
-					AbsAddress += X;
-					if (Copy_AbsAddress + X > 254)
-						Cycles--;
-					A = ReadByte(
-						AbsAddress,
-						Cycles,
-						memory);
-					LDASetStatus();
-				}break;
-				case INS_LDA_ABS_Y:
-				{
-					Word AbsAddress = FetchWord(Cycles, memory);
-					auto Copy_AbsAddress = AbsAddress % 256;
-					AbsAddress += Y;
-					if (Copy_AbsAddress + Y > 254)
-						Cycles--;
-					A = ReadByte(
-						AbsAddress,
-						Cycles,
-						memory);
-					LDASetStatus();
-				}break;
-				case INS_LDA_IND_X: 
-				{
-					Byte IndAddress = FetchByte(Cycles, memory);
-					IndAddress += X;
-					Cycles--;
-					Word ind_address_ = ReadWord(
-						IndAddress,
-						Cycles,
-						memory);
-					A = ReadByte(
-						ind_address_,
-						Cycles,
-						memory);
-					LDASetStatus();
-				}break;
-				case INS_LDA_IND_Y:
-				{
-					Byte IndAddress = FetchByte(Cycles, memory);
-					Word ind_address_ = ReadWord(
-						IndAddress,
-						Cycles,
-						memory);
-					auto Copy_ind_address_ = ind_address_ % 256;
-					ind_address_ += Y;
-					if (Copy_ind_address_ + Y > 254)
-						Cycles--;
-					A = ReadByte(
-						ind_address_,
-						Cycles,
-						memory);
-					LDASetStatus();
-				}break;
-				case INS_JSR:
-				{
-					Word SubAddress = FetchWord(Cycles, memory); //两个周期
-					memory.WriteWord(PC - 1, SP, Cycles); //两个周期
-					SP += 2;
-					PC = SubAddress;
-					Cycles--;
-				}break;
-				default:
-				{
-					printf("没有设置 0x%x 指令\n",Ins);
-					Cycles++;//由于前面的使用了一次周期读取无用数据，所以要加回来
-					goto finish;
-				}break;
-			}
-		}
-finish:
-		return CyclesRequests - Cycles ;
-	}
+	/* CPU判断指令函数*/
+	s32 Execute(s32 Cycles, Mem& memory);
 
+	/* Zero Page处理函数*/
+	Byte AddZeroPage(s32& Cycles, Mem& memory);
+	
+	/* Zero Page + X or Y 处理函数*/
+	Byte AddZeroPageAdd(s32& Cycles, Mem& memory,Byte _register);
+
+	/* Absolute address */
+	Byte AbsoluteAddress(s32& Cycles, Mem& memory);
+
+	/* Absolute address + register*/
+	Byte AbsoluteAddress_Register(s32& Cycles, Mem& memory, Byte _register);
+
+	/* Indirect address X */
+	Byte IndirectAddressX(s32& Cycles, Mem& memory);
+
+	/* Indirect address Y*/
+	Byte IndirectAddressY(s32& Cycles, Mem& memory);
 };
--- 6502/6502Test/src/6502Tests.cpp
+++ 6502/6502Test/src/6502Tests.cpp
@@ -4,8 +4,9 @@
 class M6502Test1 : public testing::Test
 {
 public:
-	Mem mem;
-	CPU cpu;
+
+	m6502::Mem mem;
+	m6502::CPU cpu;
 	virtual void SetUp()
 	{
 		cpu.Reset( mem );
@@ -14,13 +15,13 @@ public:
 	virtual void TearDown()
 	{
 	}
-	void CyclesConfirm(CPU& cpu, const Byte& DataA, const s32 Cycles, const s32 RequestCycles)
+	void CyclesConfirm( m6502::Byte *_register, const m6502::Byte& DataA, const m6502::s32 Cycles, const m6502::s32 RequestCycles)
 	{
-		s32 CyclesUsed = cpu.Execute(Cycles, mem);
-		EXPECT_EQ(cpu.A, DataA);
+		m6502::s32 CyclesUsed = cpu.Execute(Cycles, mem);
+		EXPECT_EQ(*_register, DataA);
 		EXPECT_EQ(CyclesUsed, RequestCycles);
 	}
-	void VerfiyCPUFlagCIDBV(const CPU& cpu, const CPU& CPUCopy)
+	void VerfiyCPUFlagCIDBV(const m6502::CPU& cpu, const m6502::CPU& CPUCopy)
 	{
 		EXPECT_EQ(cpu.C, CPUCopy.C);
 		EXPECT_EQ(cpu.I, CPUCopy.I);
@@ -35,45 +36,101 @@ TEST_F( M6502Test1, LDAImmediate_Can_Load_A_Value_Into_The_ARegister )
 {
 	// given:
 	// start - inline a little program
-	mem[0xFFFC] = CPU::INS_LDA_IM;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_IM;
 	mem[0xFFFD] = 0x84;
 	// end - inline a little program
 
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x84, 2, 2);
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x84, 2, 2);
 	EXPECT_FALSE( cpu.Z );
 	EXPECT_TRUE( cpu.N );
 	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
 }
+TEST_F(M6502Test1, LDXImmediate_Can_Load_A_Value_Into_The_ARegister)
+{
+	// given:
+	// start - inline a little program
+	mem[0xFFFC] = m6502::CPU::INS_LDX_IM;
+	mem[0xFFFD] = 0x84;
+	// end - inline a little program
 
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.X, 0x84, 2, 2);
+	EXPECT_FALSE(cpu.Z);
+	EXPECT_TRUE(cpu.N);
+	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
+}
+TEST_F(M6502Test1, LDYImmediate_Can_Load_A_Value_Into_The_ARegister)
+{
+	// given:
+	// start - inline a little program
+	mem[0xFFFC] = m6502::CPU::INS_LDY_IM;
+	mem[0xFFFD] = 0x84;
+	// end - inline a little program
+
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.Y, 0x84, 2, 2);
+	EXPECT_FALSE(cpu.Z);
+	EXPECT_TRUE(cpu.N);
+	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
+}
 TEST_F( M6502Test1, LDAZeroPageCanLoadAValueIntoTheARegister )
 {
 	// given:
 	// start - inline a little program
-	mem[0xFFFC] = CPU::INS_LDA_ZP;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_ZP;
 	mem[0xFFFD] = 0x42;
 	mem[0x0042] = 0x37;
 	// end - inline a little program
 
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x37, 3, 3);
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x37, 3, 3);
 	EXPECT_FALSE( cpu.Z );
 	EXPECT_FALSE( cpu.N );
 	VerfiyCPUFlagCIDBV( cpu, CPUCopy);
 }
+TEST_F(M6502Test1, LDXZeroPageCanLoadAValueIntoTheARegister)
+{
+	// given:
+	// start - inline a little program
+	mem[0xFFFC] = m6502::CPU::INS_LDX_ZP;
+	mem[0xFFFD] = 0x42;
+	mem[0x0042] = 0x37;
+	// end - inline a little program
 
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.X, 0x37, 3, 3);
+	EXPECT_FALSE(cpu.Z);
+	EXPECT_FALSE(cpu.N);
+	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
+}
+TEST_F(M6502Test1, LDYZeroPageCanLoadAValueIntoTheARegister)
+{
+	// given:
+	// start - inline a little program
+	mem[0xFFFC] = m6502::CPU::INS_LDY_ZP;
+	mem[0xFFFD] = 0x42;
+	mem[0x0042] = 0x37;
+	// end - inline a little program
+
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.Y, 0x37, 3, 3);
+	EXPECT_FALSE(cpu.Z);
+	EXPECT_FALSE(cpu.N);
+	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
+}
 TEST_F( M6502Test1, LDAZeroPageXCanLoadAValueIntoTheARegister )
 {
 	// given:
 	cpu.X = 0xff;
 	// start - inline a little program
-	mem[0xFFFC] = CPU::INS_LDA_ZPX;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_ZPX;
 	mem[0xFFFD] = 0x42;
 	mem[0x0041] = 0x37;
 	// end - inline a little program
 
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x37,4, 4);
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x37,4, 4);
 	EXPECT_FALSE( cpu.Z );
 	EXPECT_FALSE( cpu.N );
 	VerfiyCPUFlagCIDBV( cpu, CPUCopy);
@@ -82,65 +139,130 @@ TEST_F( M6502Test1, LDAZeroPageXCanLoadAValueIntoTheARegister )
 TEST_F(M6502Test1, LDAAbsoluteCanLoadAValueIntoTheARegister)
 {
 	// start - inline a little program
-	mem[0xFFFC] = CPU::INS_LDA_ABS;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_ABS;
 	mem[0xFFFD] = 0x42;
 	mem[0xFFFE] = 0x37;
 	mem[0x3742] = 0x89;
 	// end - inline a little program
 
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x89, 4, 4);
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x89, 4, 4);
 	EXPECT_FALSE(cpu.Z);
 	EXPECT_TRUE(cpu.N);
-	VerfiyCPUFlagCIDBV( cpu,  CPUCopy);
+	VerfiyCPUFlagCIDBV( cpu, CPUCopy);
+
+}
+TEST_F(M6502Test1, LDXAbsoluteCanLoadAValueIntoTheARegister)
+{
+	// start - inline a little program
+	mem[0xFFFC] = m6502::CPU::INS_LDX_ABS;
+	mem[0xFFFD] = 0x42;
+	mem[0xFFFE] = 0x37;
+	mem[0x3742] = 0x89;
+	// end - inline a little program
+
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.X, 0x89, 4, 4);
+	EXPECT_FALSE(cpu.Z);
+	EXPECT_TRUE(cpu.N);
+	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
+
+}
+TEST_F(M6502Test1, LDYAbsoluteCanLoadAValueIntoTheARegister)
+{
+	// start - inline a little program
+	mem[0xFFFC] = m6502::CPU::INS_LDY_ABS;
+	mem[0xFFFD] = 0x42;
+	mem[0xFFFE] = 0x37;
+	mem[0x3742] = 0x89;
+	// end - inline a little program
+
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.Y, 0x89, 4, 4);
+	EXPECT_FALSE(cpu.Z);
+	EXPECT_TRUE(cpu.N);
+	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
 
 }
 TEST_F(M6502Test1, LDAAbsoluteXCanLoadAValueIntoTheARegister)
 {
 	// start - inline a little program
 	cpu.X = 1;
-	mem[0xFFFC] = CPU::INS_LDA_ABS_X;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_ABS_X;
 	mem[0xFFFD] = 0xf3; // 
 	mem[0xFFFE] = 0x01;
 	mem[0x01f4] = 0x89;
 	// end - inline a little program
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x89, 4,4);
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x89, 4,4);
 
 	EXPECT_FALSE(cpu.Z);
 	EXPECT_TRUE(cpu.N);
 	VerfiyCPUFlagCIDBV( cpu, CPUCopy);
 
 }
-TEST_F(M6502Test1, LDAAbsoluteXCanLoadAValueIntoTheARegisterWhenItCrossThePage)
+TEST_F(M6502Test1, LDXAbsoluteYCanLoadAValueIntoTheARegister)
 {
 	// start - inline a little program
-	cpu.X = 2;
-	mem[0xFFFC] = CPU::INS_LDA_ABS_X;
-	mem[0xFFFD] = 0xfe; // 0xfe - 0x101 跨页了需要使用5个周期
+	cpu.Y = 1;
+	mem[0xFFFC] = m6502::CPU::INS_LDX_ABS_Y;
+	mem[0xFFFD] = 0xf3; // 
 	mem[0xFFFE] = 0x01;
-	mem[0x0200] = 0x89;
+	mem[0x01f4] = 0x89;
 	// end - inline a little program
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.X, 0x89, 4, 4);
 
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x89, 5, 5);
 	EXPECT_FALSE(cpu.Z);
 	EXPECT_TRUE(cpu.N);
 	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
 
 }
+TEST_F(M6502Test1, LDYAbsoluteXCanLoadAValueIntoTheARegister)
+{
+	// start - inline a little program
+	cpu.X = 1;
+	mem[0xFFFC] = m6502::CPU::INS_LDY_ABS_X;
+	mem[0xFFFD] = 0xf3; // 
+	mem[0xFFFE] = 0x01;
+	mem[0x01f4] = 0x89;
+	// end - inline a little program
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.Y, 0x89, 4, 4);
+
+	EXPECT_FALSE(cpu.Z);
+	EXPECT_TRUE(cpu.N);
+	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
+
+}
+TEST_F(M6502Test1, LDAAbsoluteXCanLoadAValueIntoTheARegisterWhenItCrossThePage)
+{
+	// start - inline a little program
+	cpu.X = 2;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_ABS_X;
+	mem[0xFFFD] = 0xfe; // 0xfe - 0x101 跨页了需要使用5个周期
+	mem[0xFFFE] = 0x01;
+	mem[0x0200] = 0x89;
+	// end - inline a little program
+
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x89, 5, 5);
+	EXPECT_FALSE(cpu.Z);
+	EXPECT_TRUE(cpu.N);
+	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
+}
 TEST_F(M6502Test1, LDAAbsoluteYCanLoadAValueIntoTheARegister)
 {
 	// start - inline a little program
 	cpu.Y = 1;
-	mem[0xFFFC] = CPU::INS_LDA_ABS_Y;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_ABS_Y;
 	mem[0xFFFD] = 0xf0;//
 	mem[0xFFFE] = 0x01;
 	mem[0x01F1] = 0x89;
 	// end - inline a little program
 
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x89, 4, 4);
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x89, 4, 4);
 	EXPECT_FALSE(cpu.Z);
 	EXPECT_TRUE(cpu.N);
 	VerfiyCPUFlagCIDBV(cpu,CPUCopy);
@@ -150,32 +272,66 @@ TEST_F(M6502Test1, LDAAbsoluteYCanLoadAValueIntoTheARegisterWhenItCrossThePage)
 {
 	// start - inline a little program
 	cpu.Y = 1;
-	mem[0xFFFC] = CPU::INS_LDA_ABS_Y;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_ABS_Y;
 	mem[0xFFFD] = 0xfe;//跨页了
 	mem[0xFFFE] = 0x01;
 	mem[0x01FF] = 0x89;
 	// end - inline a little program
 
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x89, 5, 5);
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x89, 5, 5);
 	EXPECT_FALSE(cpu.Z);
 	EXPECT_TRUE(cpu.N);
 	VerfiyCPUFlagCIDBV(cpu,CPUCopy);
+
+}
+TEST_F(M6502Test1, LDXAbsoluteYCanLoadAValueIntoTheARegisterWhenItCrossThePage)
+{
+	// start - inline a little program
+	cpu.Y = 1;
+	mem[0xFFFC] = m6502::CPU::INS_LDX_ABS_Y;
+	mem[0xFFFD] = 0xfe;//跨页了
+	mem[0xFFFE] = 0x01;
+	mem[0x01FF] = 0x89;
+	// end - inline a little program
+
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.X, 0x89, 5, 5);
+	EXPECT_FALSE(cpu.Z);
+	EXPECT_TRUE(cpu.N);
+	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
+
+}
+TEST_F(M6502Test1, LDYAbsoluteXCanLoadAValueIntoTheARegisterWhenItCrossThePage)
+{
+	// start - inline a little program
+	cpu.X = 1;
+	mem[0xFFFC] = m6502::CPU::INS_LDY_ABS_X;
+	mem[0xFFFD] = 0xfe;//跨页了
+	mem[0xFFFE] = 0x01;
+	mem[0x01FF] = 0x89;
+	// end - inline a little program
+
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.Y, 0x89, 5, 5);
+	EXPECT_FALSE(cpu.Z);
+	EXPECT_TRUE(cpu.N);
+	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
 
 }
 TEST_F(M6502Test1, LDAInd_XCanLoadAValueIntoTheARegister)
 {
 	// start - inline a little program
 	cpu.X = 1;
-	mem[0xFFFC] = CPU::INS_LDA_IND_X;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_IND_X;
 	mem[0xFFFD] = 0x03;
 	mem[0x0004] = 0x83;
 	mem[0x0005] = 0x84;
 	mem[0x8483] = 0x89;
 	// end - inline a little program
 
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x89, 6, 6);
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x89, 6, 6);
 	EXPECT_FALSE(cpu.Z);
 	EXPECT_TRUE(cpu.N);
 	VerfiyCPUFlagCIDBV(cpu,CPUCopy);
@@ -185,15 +341,15 @@ TEST_F(M6502Test1, LDAInd_YCanLoadAValueIntoTheARegister)
 {
 	// start - inline a little program
 	cpu.Y = 0x01;
-	mem[0xFFFC] = CPU::INS_LDA_IND_Y;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_IND_Y;
 	mem[0xFFFD] = 0x03; // 0x03
 	mem[0x0003] = 0x82;
 	mem[0x0004] = 0x84; // 0x8482 + 0x1 =  0x8483
 	mem[0x8483] = 0x89;
 	// end - inline a little program
 
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x89, 5, 5);
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x89, 5, 5);
 	EXPECT_FALSE(cpu.Z);
 	EXPECT_TRUE(cpu.N);
 	VerfiyCPUFlagCIDBV(cpu, CPUCopy);
@@ -203,15 +359,15 @@ TEST_F(M6502Test1, LDAInd_YCanLoadAValueIntoTheARegisterWhenItCroessThePage)
 {
 	// start - inline a little program
 	cpu.Y = 0xff;
-	mem[0xFFFC] = CPU::INS_LDA_IND_Y;
+	mem[0xFFFC] = m6502::CPU::INS_LDA_IND_Y;
 	mem[0xFFFD] = 0x03; // 0x03
 	mem[0x0003] = 0x82;
 	mem[0x0004] = 0x84; // 0x8284 + 0xff =  0x8581
 	mem[0x8581] = 0x89;
 	// end - inline a little program
 
-	CPU CPUCopy = cpu;
-	CyclesConfirm(cpu, 0x89,6, 6);
+	m6502::CPU CPUCopy = cpu;
+	CyclesConfirm(&cpu.A, 0x89,6, 6);
 	EXPECT_FALSE(cpu.Z);
 	EXPECT_TRUE(cpu.N);
 	VerfiyCPUFlagCIDBV(cpu,CPUCopy);
